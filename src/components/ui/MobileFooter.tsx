4340c44719f514a7752e3c1eefd74224582bc274
Semih Irmak <smhirmak@hotmail.com>
2023-04-26 02:16:42 +0300

    test bottomnavigation

--- src/components/ui/MobileFooter.tsx
+++ src/components/ui/MobileFooter.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Box, BottomNavigation, Paper } from '@mui/material';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
+import CategoryIcon from '@mui/icons-material/Category';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { BottomNavigation, Paper } from '@mui/material';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import React from 'react';
 
 const MobileFooter = () => {
-  const [value, setValue] = React.useState(0);
   return (
-    <Paper
-      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: '100%', zIndex: 99 }}
-      elevation={3}>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: '100%' }} elevation={3}>
       <BottomNavigation showLabels>
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Category" icon={<CategoryIcon />} />
+        <BottomNavigationAction label="Basket" icon={<ShoppingCartIcon />} />
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
       </BottomNavigation>
     </Paper>
