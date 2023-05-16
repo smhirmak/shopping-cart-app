54658dd65d74ff58c0cee2c05cc99cf83f95be2b
Semih Irmak <smhirmak@hotmail.com>
2023-05-16 17:03:11 +0300

    solve some bug

--- src/axios.ts
+++ src/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const AxiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  timeout: 3000
-});
-
 // export const AxiosClient = axios.create({
 //   baseURL: 'http://localhost:3000',
 //   timeout: 3000
 // });
+
+export const AxiosClient = axios.create({
+  baseURL: 'https://shopping-cart-app-xi.vercel.app/',
+  timeout: 3000
+});
