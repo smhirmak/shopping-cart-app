3f8158af69f3b668a5275f2dbf12cf4e20101e58
Semih Irmak <smhirmak@hotmail.com>
2023-05-16 17:20:16 +0300

    add category page title

--- src/pages/category/[categoryName]/index.tsx
+++ src/pages/category/[categoryName]/index.tsx
@@ -3,11 +3,21 @@ import { IProduct } from '@/types/IProduct';
 import { Container, Grid } from '@mui/material';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 import React from 'react';
 
-const Category: React.FC<{ searchCategoryResponse: IProduct[] }> = ({ searchCategoryResponse }) => {
+const Category: React.FC<{ searchCategoryResponse: IProduct[]; params: string }> = ({
+  searchCategoryResponse,
+  params
+}) => {
   return (
     <Container maxWidth={'xl'}>
+      <Head>
+        <title>{params.charAt(0).toUpperCase() + params.slice(1)}</title>
+        <meta name="description" content="Shopping cart app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="shop-ico.png" />
+      </Head>
       <Grid container spacing={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
         <ProductList items={searchCategoryResponse} />
       </Grid>
@@ -32,7 +42,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   return {
     props: {
-      searchCategoryResponse
+      searchCategoryResponse,
+      params
     }
   };
 };
