f57616bf11cd7e417a2debfd38e32a36ebae24de
Semih Irmak <smhirmak@hotmail.com>
2023-09-01 19:38:23 +0300

    for search bug

--- src/pages/api/categories.tsx
+++ src/pages/api/categories.tsx
@@ -11,7 +11,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       })
       .catch((error) => {
         reject(error);
-        return res.status(error.response.status).json(error.response.data);
+        return res.status(404).json(error.response.data);
       });
   });
 }
