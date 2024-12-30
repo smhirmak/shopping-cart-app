18a7aac0f7b1a3a2f6019b03a0f19ef319e5a843
Muhammed Semih Irmak <42941265+smhirmak@users.noreply.github.com>
2024-12-30 21:09:56 +0300

    edit favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: React.FC<{ products: IProduct[] | any }> = ({ products }) => {
         <title>Shopping Cart App</title>
         <meta name="description" content="Shopping cart app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="shop-ico.png" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <Grid container spacing={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
         {products.error ? <Typography>{products.error}</Typography> : <ProductList items={items} />}
